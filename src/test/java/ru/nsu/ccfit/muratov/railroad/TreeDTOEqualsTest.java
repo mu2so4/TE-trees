99b8121dbe8f2b30d4ee6e96ddfbb5cfa3c6c430
mu2so4 <b4.orangutang@gmail.com>
2023-08-19 17:00:04 +0700

    Add reflexivity equals test

--- src/test/java/ru/nsu/ccfit/muratov/railroad/TreeDTOEqualsTest.java
+++ src/test/java/ru/nsu/ccfit/muratov/railroad/TreeDTOEqualsTest.java
@@ -1,2 +1,15 @@
-package ru.nsu.ccfit.muratov.railroad;public class TreeDTOEqualsTest {
+package ru.nsu.ccfit.muratov.railroad;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TreeDTOEqualsTest {
+    @Test
+    public void testReflexivity() {
+        TreeDTO dto = new TreeDTO();
+        for(int index = 0; index < 100; index++) {
+            dto.setId(index);
+            Assert.assertEquals(dto, dto);
+        }
+    }
 }
