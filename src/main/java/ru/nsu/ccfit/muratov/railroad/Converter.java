823144cc949676b9d75cdd27160b9be778b3e68f
mu2so4 <b4.orangutang@gmail.com>
2023-08-19 09:16:10 +0700

    Add solution created before the end of the interview

--- src/main/java/ru/nsu/ccfit/muratov/railroad/Converter.java
+++ src/main/java/ru/nsu/ccfit/muratov/railroad/Converter.java
@@ -1,12 +1,25 @@
 package ru.nsu.ccfit.muratov.railroad;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class Converter {
     public static Collection<TreeDTO> convert(Collection<TreeEntity> entities) {
-        //TODO
-        return null;
+        List<TreeDTO> roots = new ArrayList<>();
+        Map<Integer, TreeDTO> forest = new HashMap<>();
+        for(TreeEntity entity: entities) {
+            TreeDTO dto = new TreeDTO();
+            dto.children = new ArrayList<>();
+            dto.id = entity.id;
+            dto.name = entity.name;
+            //TODO getting direct children from map
+            if(entity.parentId == null) {
+                //...
+                forest.put(entity.id, dto);
+                roots.add(dto);
+            }
+            //TODO getting suptree in map
+        }
+        return roots;
     }
 
     private static class TreeEntity {
