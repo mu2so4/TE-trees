e750217c8dcfae0864d0bc7c51b8b54a34b40895
mu2so4 <b4.orangutang@gmail.com>
2023-08-19 13:09:59 +0700

    Write first version of converter

--- src/main/java/ru/nsu/ccfit/muratov/railroad/Converter.java
+++ src/main/java/ru/nsu/ccfit/muratov/railroad/Converter.java
@@ -5,19 +5,28 @@ import java.util.*;
 public class Converter {
     public static Collection<TreeDTO> convert(Collection<TreeEntity> entities) {
         List<TreeDTO> roots = new ArrayList<>();
-        Map<Integer, TreeDTO> forest = new HashMap<>();
+        Map<Integer, TreeDTO> entitiesById = new HashMap<>();
         for(TreeEntity entity: entities) {
-            TreeDTO dto = new TreeDTO();
-            dto.children = new ArrayList<>();
-            dto.id = entity.id;
-            dto.name = entity.name;
-            //TODO getting direct children from map
-            if(entity.parentId == null) {
-                //...
-                forest.put(entity.id, dto);
-                roots.add(dto);
+            TreeDTO dto = entitiesById.get(entity.id);
+            if (dto == null) {
+                dto = new TreeDTO();
+                dto.id = entity.id;
+                dto.children = new ArrayList<>();
             }
-            //TODO getting suptree in map
+            dto.name = entity.name;
+
+            entitiesById.put(entity.id, dto);
+            if(entity.parentId == null) {
+                roots.add(dto);
+                continue;
+            }
+            TreeDTO directParent = entitiesById.get(entity.parentId);
+            if(directParent == null) {
+                directParent = new TreeDTO();
+                directParent.children = new ArrayList<>();
+                directParent.id = entity.parentId;
+            }
+            directParent.children.add(dto);
         }
         return roots;
     }
